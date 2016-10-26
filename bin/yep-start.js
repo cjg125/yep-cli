5d011bcbc221f1dbd1ccf4c62c691027dc3d543b
caojungang <caojungang@sogou-inc.com>
2016-10-26 18:56:02 +0800

    添加yep start

--- bin/yep-start.js
+++ bin/yep-start.js
@@ -1,3 +1,40 @@
 #!/usr/bin/env node
 
-console.log('hehe')
+const program = require('commander')
+const chalk = require('chalk')
+const nodemon = require('nodemon')
+const path = require('path')
+const log = require('../lib/log')
+const port = Number.parseInt(process.argv[2]) || 8888
+
+program
+  .usage('[options] [port]')
+  .allowUnknownOption()
+  .on('--help', function() {
+    console.log('  Examples:')
+    console.log(chalk.gray('    #  start http server,port => default:8888'))
+    console.log('    $ yep start 8888')
+    console.log()
+  })
+  .parse(process.argv)
+var pid = process.pid
+nodemon({
+  script: path.resolve(__dirname, '..', 'lib'),
+  ext: 'html',
+  args: process.argv.slice(2),
+  "watch": [
+    "template"
+  ]
+}).on('start', function() {
+  // log('App has started')
+  log('App listening on port ' + port)
+}).on('restart', function(files) {
+  if (!files) return
+  log('pid (' + pid + ' ' + process.pid + ')')
+  log('App restarted due to ' + files)
+}).on('quit', function() {
+  // process.exit(0)
+  process.kill(process.pid, 'SIGUSR2')
+}).on('log', function(data) {
+  // log(data.message)
+})

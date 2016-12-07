4e0681c218f8120decc09c8ef0acd6f46c07e081
caojungang <caojungang@sogou-inc.com>
2016-12-07 13:54:03 +0800

    添加build

--- bin/yep-build.js
+++ bin/yep-build.js
@@ -1,3 +1,26 @@
 #!/usr/bin/env node
 
-console.log('hehe')
+const { resolve } = require('path')
+const fork = require('child_process').fork
+const gulpcli = resolve(__dirname, '..', 'node_modules', 'gulp-cli', 'bin', 'gulp.js')
+const gulpfile = resolve(__dirname, '..', 'lib', 'gulpfile.js')
+const chalk = require('chalk')
+const program = require('commander')
+program
+  .on('--help', function() {
+    console.log('  Examples:')
+    console.log(chalk.gray('    # yep build'))
+  })
+  .parse(process.argv)
+
+
+function worker() {
+  return new Promise((resolve, reject) => {
+    let forker = fork(gulpcli, ['--gulpfile', gulpfile, '--cwd', process.cwd(), '--color'])
+    forker.on('close', (code) => {
+      resolve(true)
+    })
+  })
+}
+
+worker()

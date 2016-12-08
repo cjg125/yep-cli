1750d11b6c60c4df3fa0e8c571843311cc43450d
caojungang <caojungang@sogou-inc.com>
2016-12-08 18:49:39 +0800

    init命令优化

--- bin/yep-init.js
+++ bin/yep-init.js
@@ -7,66 +7,62 @@ const program = require('commander')
 const chalk = require('chalk')
 const inquirer = require('inquirer')
 const download = require('download-git-repo')
-const template = require('../lib/tmpl.config')
+const tmpls = require('../lib/tmpl.config')
 
 program
+  .usage('[template-name] [project-name] [options]')
   .option('-c, --clone', 'use git clone')
   .on('--help', function() {
     console.log('  Examples:')
     console.log(chalk.gray('    # create a new project'))
     console.log('    $ yep init')
-    console.log(chalk.gray('    # create a new project use git clone'))
+    console.log(chalk.gray('    # create a new project using git clone'))
     console.log('    $ yep init -c')
+    console.log(chalk.gray('    # created a project using command line'))
+    console.log('    $ yep init template-name project-name -c')
     console.log()
   })
   .parse(process.argv)
 
 
 let dirname = process.cwd()
-let projectConfig = {}
 let clone = program.clone || false
 
-inquirer.prompt([{
-  type: 'input',
-  name: 'name',
-  message: 'Please enter a project name:',
-  validate: (input) => {
-    if (input == '') {
-      return 'Please enter a project name'
-    }
-    return new Promise((resolve, reject) => {
-      fs.stat(join(dirname, input), (err, stats) => {
-        // stats.isDirectory()
-        return resolve(err ? true : 'Project already exists')
-      })
-
+function exist(path, message = '') {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      return resolve(err ? true : message)
     })
-  }
-}]).then((answers) => {
-  projectConfig.name = answers.name
-  chooseTemplate()
-})
-
-
-
-function chooseTemplate() {
-  inquirer.prompt([{
-    type: 'list',
-    name: 'template',
-    message: 'Choose a template for your new project',
-    choices: Object.keys(template)
-  }]).then(function(answers) {
-    projectConfig.template = answers.template
-    generate(projectConfig)
   })
 }
 
-function generate(options) {
-  let name = join(dirname, options.name)
-  let tpl = template[options.template]
-  let ui = installation()
 
-  download(tpl, name, { clone: clone }, (err) => {
+function projectName() {
+  return inquirer.prompt([{
+    type: 'input',
+    name: 'value',
+    message: 'Please enter a project name:',
+    validate: (input) => {
+      if (input == '') {
+        return 'Please enter a project name'
+      }
+      return exist(join(dirname, input), 'Project already exists')
+    }
+  }])
+}
+
+function templateName() {
+  return inquirer.prompt([{
+    type: 'list',
+    name: 'value',
+    message: 'Choose a template for your new project',
+    choices: Object.keys(tmpls)
+  }])
+}
+
+function generate(opts) {
+  let ui = installation()
+  download(opts.tpl, opts.name, { clone: clone }, (err) => {
     if (err) {
       ui.updateBottomBar(chalk.red(err))
     } else {
@@ -100,3 +96,43 @@ function installation() {
     }
   }
 }
+
+
+
+async function co() {
+  let args = program.args
+
+  let tpl, name
+
+  if (args.length == 0) {
+    name = await projectName()
+    tpl = await templateName()
+    name = name.value
+    tpl = tpl.value
+  } else {
+    tpl = args[0]
+    name = args[1] || tpl + (+new Date)
+  }
+
+  tpl = tmpls[tpl] || tpl
+  name = join(dirname, name)
+
+  let message = await exist(name, false)
+
+  // console.log(tpl)
+  // console.log(name)
+  // console.log(message)
+
+
+  if (message === false) { // 存在
+    console.log('Project already exists')
+    return
+  }
+
+  generate({
+    tpl: tpl,
+    name: name
+  })
+
+}
+co()
